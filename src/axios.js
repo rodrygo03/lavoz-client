7c62da5e2006c30d2715960140e7eda4ded267e5
Anna <ambrooks.271@tamu.edu>
2024-07-01 14:54:21 -0500

    removed heic from accept

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const makeRequest = axios.create({
-    //baseURL: "https://server.postsstation.com/api/",
-    baseURL: "http://localhost:8800/api/",
+    baseURL: "https://server.postsstation.com/api/",
+    //baseURL: "http://localhost:8800/api/",
     withCredentials: true,
 });
