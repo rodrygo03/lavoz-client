a5fafa5d9fb86aa27090491e140aafc2bd37f618
rodrigoorozco <rodrigoorozco025@gmail.com>
2025-06-07 13:44:53 -0500

    LAN support for development

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const isProduction = process.env.REACT_APP_IS_PRODUCTION === "true";
+// const isProduction = process.env.REACT_APP_IS_PRODUCTION === "true";
+const isProduction = true;
 
 export const makeRequest = axios.create({
     baseURL: isProduction ? "https://server.postsstation.com/api/" : process.env.REACT_APP_NETWORK_ADDR,
