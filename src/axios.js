3d3d12362c2e8121290dce887c99d7a53308e0dc
rodrigoorozco <rodrigoorozco025@gmail.com>
2025-06-24 20:49:34 -0500

    fix request

--- src/axios.js
+++ src/axios.js
@@ -1,17 +1,17 @@
 import axios from "axios"
 
-const isLocalHost = window.location.hostname === "localhost";
-const isProduction = process.env.REACT_APP_IS_PRODUCTION === "true";
+// const isLocalHost = window.location.hostname === "localhost";
+// const isProduction = process.env.REACT_APP_IS_PRODUCTION === "true";
 
-const baseURL = isProduction ? "https://server.postsstation.com/api/" : 
-    isLocalHost ? "http://localhost:8800/api" : process.env.REACT_APP_NETWORK_ADDR;
-
-export const makeRequest = axios.create({
-    baseURL,
-    withCredentials: true,
-});
+// const baseURL = isProduction ? "https://server.postsstation.com/api/" : 
+//     isLocalHost ? "http://localhost:8800/api" : process.env.REACT_APP_NETWORK_ADDR;
 
 // export const makeRequest = axios.create({
-//     baseURL: "https://server.postsstation.com/api/",
+//     baseURL,
 //     withCredentials: true,
 // });
+
+export const makeRequest = axios.create({
+    baseURL: "https://server.postsstation.com/api/",
+    withCredentials: true,
+});
